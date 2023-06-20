473b433dfe2fa00d34bca6b5026b90bdc91778a0
Ario-sut <rakaario6@gmail.com>
2023-06-20 03:07:00 -0700

    Comments Optimized

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import os
-import shutil #utiliuty, function, directory trees
-import sys
-import cv2
+import os  
+import shutil  
+import sys  
+import cv2  
 from datetime import datetime
 
-class framecapture:
+
+class FrameCapture:
     def __init__(self, video_path):
-        self.directory = "captured_frames-%s"%datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        self.directory = f"captured_frames-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
         self.video = cv2.VideoCapture(video_path)
         if os.path.exists(self.directory):
-            shutil.rmtree(self.directory)
-        os.mkdir(self.directory)
+            shutil.rmtree(self.directory)  # Remove the existing directory if it already exists
+        os.mkdir(self.directory)  # Create a new directory to store the captured frames
 
     def capture_frames(self):
         total_frames = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -19,26 +20,13 @@ class framecapture:
             success, image = self.video.read()
             if not success:
                 break
-            capture = f"{self.directory}/frame%d.jpg"%count
-            count+=1
+            capture = f"{self.directory}/frame{count}.jpg"  # Define the file path for the captured frame
+            count += 1
             progress = (count + 1) / total_frames * 100
-            print(f"Progress: {progress:.2f}% ({count + 1}/{total_frames})", end="\r")
-            
-            cv2.imwrite(capture, image)
+            print(f"Progress: {progress:.2f}% ({count + 1}/{total_frames})", end="\r")  # Print the capture progress
+            cv2.imwrite(capture, image)  # Save the captured frame as an image file
 
 if __name__ == '__main__':
-    file_path = sys.argv[1]
-    fc = framecapture(file_path)
-    fc.capture_frames()
-
-'''
-    def capture_frames(self):
-        cv2.object = cv2.VideoCapture(self.file_path)
-        frame_number =0
-        frame_found=1
-        while frame_found:
-            frame_found, image= cv2.object.read()
-            capture = f'{self.directory}/frame{frame_number}.jpg'
-            cv2.imwrite(capture,image)
-            frame_number +=1
-'''
+    file_path = sys.argv[1]  # Get the video file path from the command line argument
+    fc = FrameCapture(file_path)  # Create an instance of the FrameCapture class
+    fc.capture_frames()  # Start capturing the frames from the video
